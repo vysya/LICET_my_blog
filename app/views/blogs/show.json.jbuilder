2bc6fae83871bab86c1250b20ad4d3f5d6e1f49a
vysya <vysya.francis@gmail.com>
2015-02-24 15:03:42 +0530

    my_first_blog

---
+++ app/views/blogs/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @blog, :id, :title, :content, :author_name, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :blogs
+
+  get "home1"=> "home#home"
+  get "about"=>"home#about"
+  get "contact"=>"home#contact"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
